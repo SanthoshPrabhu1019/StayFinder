f9ae2bddd5eaf05117e94b4063c2f0260d27c319
Santhosh Prabhu <santhoshprabhu1019@gmail.com>
2025-08-28 00:17:56 +0530

    Toggle for displaying Taxes

--- public/js/script.js
+++ public/js/script.js
@@ -79,3 +79,20 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+  let taxSwitch =document.getElementById("flexSwitchCheckDefault");
+   taxSwitch.addEventListener("click",()=>{
+    let taxInfo = document.getElementsByClassName("tax-info");
+    for(info of taxInfo){
+      if(info.style.display!="inline"){
+      info.style.display ="inline";
+          }
+          else{
+            info.style.display ="none"
+          }   
+         }
+     });
+});
